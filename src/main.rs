8a581f0e1eff26f376419df5651fc51171b43b92
yuniruyuni <yuniruyuni@gmail.com>
2021-06-08 09:44:10 +0900

    implement set_cursor() and insert()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem::take;
+
 struct GapBuffer<T> {
     gap_beg: usize,
     gap_end: usize,
@@ -16,12 +18,105 @@ impl<T: Default> GapBuffer<T> {
             buffer: buffer,
         }
     }
+
+    fn cap(&self) -> usize {
+        self.buffer.len()
+    }
+
+    fn gap_cap(&self) -> usize {
+        self.gap_end - self.gap_beg
+    }
+
+    fn len(&self) -> usize {
+        self.cap() - self.gap_cap()
+    }
+
+    fn set_cursor(&mut self, pos: usize) {
+        assert!(pos < self.len());
+
+        if self.gap_beg == pos {
+            // none to do
+            return;
+        }
+
+        let gap_beg = self.gap_beg;
+        let gap_end = self.gap_end;
+        let gap_cap = self.gap_cap();
+
+        let mut copy = |src_beg, dst_beg, len| {
+            for i in 0..len {
+                let src = src_beg + i;
+                let dst = dst_beg + i;
+                self.buffer[dst] = take(&mut self.buffer[src]);
+            }
+        };
+
+        if pos < gap_beg {
+            // copy [pos, gap_beg] to [gap_end - (pos - gap_beg), gap_end]
+            let cpy_len = gap_beg - pos;
+            let src_beg = pos;
+            let dst_beg = gap_end - cpy_len;
+            copy(src_beg, dst_beg, cpy_len);
+        } else if gap_beg <= pos && pos < gap_end {
+            // copy [gap_end - (pos - gap_beg), gap_end] to [gap_beg, pos]
+            let cpy_len = pos - gap_beg;
+            let src_beg = gap_end - cpy_len;
+            let dst_beg = gap_beg - cpy_len;
+            copy(src_beg, dst_beg, cpy_len);
+        } else {
+            // copy [gap_end - (pos - gap_beg), gap_end] to [gap_beg, pos]
+            let cpy_len = pos - gap_beg;
+            let src_beg = gap_end;
+            let dst_beg = gap_beg;
+            copy(src_beg, dst_beg, cpy_len);
+        }
+
+        self.gap_beg = pos;
+        self.gap_end = pos + gap_cap;
+    }
+
+    fn insert(&mut self, t: T) {
+        self.extend_gap();
+
+        self.buffer[self.gap_beg] = t;
+        self.gap_beg += 1;
+    }
+
+    fn extend_gap(&mut self) {
+        if self.gap_beg < self.gap_end {
+            return;
+        }
+
+        let cur_len = self.buffer.len();
+        let new_len = cur_len * 2;
+        let mut newbuf = Vec::with_capacity(new_len);
+
+        for i in 0..self.gap_beg {
+            newbuf.push(take(&mut self.buffer[i]));
+        }
+
+        for _ in cur_len..new_len {
+            newbuf.push(T::default());
+        }
+
+        for i in self.gap_end..cur_len {
+            newbuf.push(take(&mut self.buffer[i]));
+        }
+
+        self.buffer = newbuf;
+        self.gap_end = self.gap_beg + (new_len - cur_len);
+    }
 }
 
 
 fn main() {
-    let _: GapBuffer<i32> = GapBuffer::new(100);
-    println!("Hello, world!");
+    let mut buf: GapBuffer<i32> = GapBuffer::new(2);
+    buf.insert( 1 );
+    buf.insert( 2 );
+    buf.insert( 3 );
+    buf.set_cursor( 1 );
+    buf.insert( 4 );
+    println!("{:?}", buf.buffer);
 }
 
 #[cfg(test)]
@@ -33,6 +128,39 @@ mod test {
         let buf: GapBuffer<i32> = GapBuffer::new(100);
         assert_eq!(buf.gap_beg, 0);
         assert_eq!(buf.gap_end, 100);
+        assert_eq!(buf.gap_cap(), 100);
+        assert_eq!(buf.cap(), 100);
+        assert_eq!(buf.len(), 0);
         assert_eq!(buf.buffer, vec![0; 100]);
+    }
+
+    #[test]
+    fn insert() {
+        let mut buf: GapBuffer<i32> = GapBuffer::new(2);
+        buf.insert(1);
+        assert_eq!(buf.gap_beg, 1);
+        assert_eq!(buf.gap_end, 2);
+        assert_eq!(buf.buffer, vec![1, 0]);
+    }
+
+    #[test]
+    fn insert_extend() {
+        let mut buf: GapBuffer<i32> = GapBuffer::new(2);
+        buf.insert(1);
+        buf.insert(2);
+        buf.insert(3);
+        assert_eq!(buf.gap_beg, 3);
+        assert_eq!(buf.gap_end, 4);
+        assert_eq!(buf.buffer, vec![1, 2, 3, 0]);
+    }
+
+    #[test]
+    fn set_cursor() {
+        let mut buf: GapBuffer<i32> = GapBuffer::new(2);
+        buf.insert(1);
+        buf.set_cursor(0);
+        assert_eq!(buf.gap_beg, 0);
+        assert_eq!(buf.gap_end, 1);
+        assert_eq!(buf.buffer, vec![0, 1]);
     }
 }
